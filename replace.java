44699c07b11622dfe4bc5b52bf3b6aac299f019d
vishalfromvirali <vishalfromvirali@gmail.com>
2024-11-07 01:08:09 +0530

    Add files via upload

---
+++ replace.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class replace{
+    public static void main(String[]args){
+        String str1 = new  String();
+        str1 =str1+"hello vishal cheif";
+        
+        String str2 = new String("vishal");
+        String result1 = str1.replace("hello","hi");
+
+        System.out.println(result1);
+
+        
+}
+}
